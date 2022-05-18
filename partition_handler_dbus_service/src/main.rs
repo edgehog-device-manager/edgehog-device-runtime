5d309f51b1ceff5072eea62583c3093543b46df3
Francesco Vaiani <francesco.vaiani@secomind.com>
2022-05-18 17:34:42 +0200

    Add partition manager reference dbus service
    Add a reference dbus service that exposes info and capabilities of the
    software used by EdgehogOS to manage the partition switch operation and
    info.
    This project serves as a reference for the dbus service name, path,
    interface and function names.
    
    Signed-off-by: Francesco Vaiani <francesco.vaiani@secomind.com>

--- partition_handler_dbus_service/src/main.rs
+++ partition_handler_dbus_service/src/main.rs
@@ -1,3 +1,127 @@
-fn main() {
-    println!("Hello, world!");
+/*
+ * This file is part of Edgehog.
+ *
+ * Copyright 2022 SECO Mind Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+use clap::Parser;
+use std::process::Command;
+use zbus::{dbus_interface, ConnectionBuilder};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[clap(short, long, required = true)]
+    partition_path: String,
+}
+
+struct PartitionHandler {
+    partition_path: String,
+}
+
+#[dbus_interface(name = "io.edgehog.PartitionHandler1")]
+impl PartitionHandler {
+    // Get boot consecutive fails
+    fn get_consecutive_fail_boot(&self) -> String {
+        self.get_value_for_key_from_info(&self.get_info_stdout(), "Consecutive fail boot")
+    }
+
+    // Get fail counter state
+    fn get_boot_fail_counter_state(&self) -> String {
+        self.get_value_for_key_from_info(&self.get_info_stdout(), "Boot fail counter state")
+    }
+
+    fn get_value_for_key_from_info(&self, stdout: &str, key: &str) -> String {
+        let stdout: Vec<String> = stdout
+            .lines()
+            .filter(|line| line.starts_with(key))
+            .map(|line| line.split(":").nth(1).unwrap().to_owned())
+            .collect();
+
+        stdout.first().unwrap().trim().to_string()
+    }
+
+    fn switch_partition(&self) -> bool {
+        let result = Command::new("fw_sysdata")
+            .args(["-t", "switchslot"])
+            .arg("-s")
+            .args(["-d", &self.partition_path])
+            .status()
+            .expect("Failed to execute fw_sysdata");
+
+        result.success()
+    }
+
+    fn get_info_stdout(&self) -> String {
+        String::from_utf8(
+            Command::new("fw_sysdata")
+                .args(["-t", "switchslot"])
+                .arg("-p")
+                .args(["-d", &self.partition_path])
+                .output()
+                .expect("Failed to execute fw_sysdata")
+                .stdout,
+        )
+        .expect("Failed to read output")
+    }
+}
+
+// Simple DBUS service that exposes some info and the switch partition function of fw_sysdata
+#[tokio::main]
+async fn main() -> zbus::Result<()> {
+    let Cli { partition_path } = Parser::parse();
+
+    let partition_handler = PartitionHandler { partition_path };
+
+    ConnectionBuilder::session()?
+        .name("io.edgehog.PartitionHandler")?
+        .serve_at("/io/edgehog/PartitionHandler", partition_handler)?
+        .build()
+        .await?;
+
+    loop {
+        std::thread::park()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PartitionHandler;
+
+    #[test]
+    fn partition_handler_info_test() {
+        let partition_handler = PartitionHandler {
+            partition_path: "".to_string(),
+        };
+
+        let mock_output = "Switchslot - current state:
+Boot from slot @: 00002
+Boot from slot A: 00001
+Current boot slot: B
+Previous boot slot:
+Consecutive fail boot: 0
+Boot fail counter state: disabled";
+
+        assert_eq!(
+            partition_handler.get_value_for_key_from_info(mock_output, "Consecutive fail boot"),
+            "0".to_string()
+        );
+        assert_eq!(
+            partition_handler.get_value_for_key_from_info(mock_output, "Boot fail counter state"),
+            "disabled".to_string()
+        );
+    }
 }
