09e29613b6adce8d521214fcd4ed613d28bd72b7
Joshua Chapman <joshua.chapman@secomind.com>
2026-09-15 13:57:42 +0200

    chore(interfaces): further deperecate the interfaces_dir option
    Add explicit deprectate macros and remove for the runtime options.
    
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,12 @@
 // This file is part of Edgehog.
 //
-// Copyright 2024 SECO Mind Srl
+// Copyright 2024, 2026 SECO Mind Srl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -75,6 +75,7 @@ pub enum Command {
 pub struct SharedArgs {
     /// Directory containing the Astarte interfaces.
     #[arg(short, long, env = "EDGEHOG_INTERFACES_DIR")]
+    #[deprecated(note = "the interfaces are included in the binary")]
     pub interfaces_dir: Option<PathBuf>,
     /// Directory used to retain configurations and other persistent data.
     #[arg(short, long, env = "EDGEHOG_STORE_DIR")]
